ef0114a770094bbcbf551e47f3c4c73679a01fdd
LI_FA_LIANG <274750134@qq.com>
2020-10-22 16:47:05 +0800

    1

--- config/database.js
+++ config/database.js
@@ -5,16 +5,16 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         client: 'mongo',
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', '47.113.81.85'),
         port: env.int('DATABASE_PORT', 27017),
         database: env('DATABASE_NAME', 'blog'),
         username: env('DATABASE_USERNAME', 'liang'),
-        password: env('DATABASE_PASSWORD', '196420'),
+        password: env('DATABASE_PASSWORD', '196420')
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE'),
-        ssl: env('DATABASE_SSL'),
-      },
-    },
-  },
-});
+        ssl: env('DATABASE_SSL')
+      }
+    }
+  }
+})
